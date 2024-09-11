7b47215e85cfbc3d3cb6a2d6862535d3a9b82133
Samarth8875 <21uec114@lnmiit.ac.in>
2024-09-11 11:02:41 +0000

    datatypes

---
+++ 03_basics/03_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+3) // we are using nodejs not browser so this will give error
+console.log(3+3); console.log("samarth")
+console.log(3+3)
+console.log("samarth")
+
+let name = "Samarth"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string = ""
+// boolean = false
+// null= standalone value
+// undefined
+// symbol = used to find uniqueness
+
+
+// object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
