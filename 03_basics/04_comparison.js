9b88fc559ece71437df0e25ae21d59d3d4aba3f6
Samarth8875 <21uec114@lnmiit.ac.in>
2024-09-12 11:29:05 +0000

    datatypes summary

---
+++ 03_basics/04_comparison.js
@@ -0,0 +1,13 @@
+// console.log(null >= 0)//true
+// console.log(null == 0)//false
+// console.log( null > 0 );//false
+
+const i = Symbol("123");
+const id = Symbol("123");
+
+console.log(i == id);//false
+
+const hero =[33 , 64 , 55];
+console.log(hero);
+
+
