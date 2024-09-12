b2d663aa6d8b093f1cc463bd51d8e1272ffadb8c
Samarth8875 <21uec114@lnmiit.ac.in>
2024-09-12 17:39:19 +0000

    read string functions

---
+++ 03_basics/05_strings.js
@@ -0,0 +1,15 @@
+const name = "samarth"
+const age = 21
+// console.log(`hello my name is ${name} and my age is ${age}`);
+
+const gameName ='samarthsa'
+// console.log(gameName.toUpperCase());
+// console.log(gameName)
+// console.log(gameName.charAt(5))
+// console.log(gameName.indexOf('th'))
+
+const anotherstring = gameName.slice(-9,8)
+console.log(anotherstring);
+
+
+
