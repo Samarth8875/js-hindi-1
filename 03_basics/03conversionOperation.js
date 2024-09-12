a4dd6c26b05d2a37b67c86675dec0f70f7474332
Samarth8875 <21uec114@lnmiit.ac.in>
2024-09-12 09:51:57 +0000

    conversionofDataTypes

---
+++ 03_basics/03conversionOperation.js
@@ -0,0 +1,25 @@
+let score = undefined
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber )//output will be number
+console.log(valueInNumber )//output will be NaN~Not a Number
+
+//"33"=33
+//"33abc"=NaN
+//true=1; false=0
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+//""=false
+//"samarth"=true
+
+let integer = 33
+
+let stringinteger = String(integer)
+
+console.log(stringinteger)
+console.log(typeof stringinteger)
+
