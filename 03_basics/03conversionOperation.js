3e3a07dba502c40915dce9f49a49ab7c7a05caa4
Samarth8875 <21uec114@lnmiit.ac.in>
2024-09-12 10:49:45 +0000

    string to number

--- 03_basics/03conversionOperation.js
+++ 03_basics/03conversionOperation.js
@@ -1,9 +1,9 @@
 let score = undefined
-console.log(typeof score)
+//console.log(typeof score)
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber )//output will be number
-console.log(valueInNumber )//output will be NaN~Not a Number
+//console.log(typeof valueInNumber )//output will be number
+//console.log(valueInNumber )//output will be NaN~Not a Number
 
 //"33"=33
 //"33abc"=NaN
@@ -11,7 +11,7 @@ console.log(valueInNumber )//output will be NaN~Not a Number
 
 let isLoggedIn = 1
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn)
+// console.log(booleanisLoggedIn)
 
 //""=false
 //"samarth"=true
@@ -20,6 +20,41 @@ let integer = 33
 
 let stringinteger = String(integer)
 
-console.log(stringinteger)
-console.log(typeof stringinteger)
+// console.log(stringinteger)
+// console.log(typeof stringinteger)
+
+
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2%2);
+// console.log(2*2);
+// console.log(2/2);
+
+// let str1="hello"
+// let str2 = " samarth"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(1 + 2 + "2") //32
+
+// let x = 1 + 2 + "2"
+// console.log( x);
+
+// console.log("1"+2+2); //122
+
+console.log(+true);
+
+let num1,num2,num3
+
+num1 = num2 =num3 = 2+2
+console.log(num1,num2);
+
+
+
 
